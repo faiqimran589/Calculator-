a92dea0aecb7e85f726758ccc68a4f4cf887b9b8
faiqimran589 <faiqimran589@gmail.com>
2026-07-13 18:03:48 +0500

    Add calculator program

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=int(input('Enter your first number:'))
+b=int(input('Enter your second number:'))
+c=input('Enter an arithmetic operation(+,-,*,/):')
+if c == '+' :
+   print('Addition of a and b is:',a+b)
+elif c == '-':
+   print('Subtraction of a and b is:',a-b)
+elif c == '*':
+   print('Multiplication of a and b is:',a*b)
+elif c == '/':
+   if b == 0 :
+      print('Error:Division by zero generates math error')
+   else:
+    print('Division of a and b is:',a/b)
+else:
+   print('unrecognized')
